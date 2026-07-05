2bfee9431695a3fbcee75ea956f9045119f046e0
Michael Ivan <mic23756@gmail.com>
2026-07-05 17:50:29 +0100

    Remove import statement for API_KEY and set it as a plaintext constant in script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-import { API_KEY } from './config.js'
+//import { API_KEY } from './config.js'
+
+//Key in plaintext because the project does not use a server.
+const API_KEY = '82f1bb21a99b19d84549fea528515704' // gitleaks:allow
 
 const cityInput = document.getElementById('city-input')
 const searchBtn = document.getElementById('search-btn')
